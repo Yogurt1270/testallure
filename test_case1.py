83c232f1ef85efa64d71bdc7b208c1a5b89f27a4
lidong <yogurt1270@163.com>
2017-07-27 19:16:19 +0800

    add allure note

--- test_case1.py
+++ test_case1.py
@@ -1,10 +1,14 @@
 #!/usr/local/bin
 # coding:utf-8
 
+import pytest
+
 
 class Test(object):
+    @pytest.allure.severity(pytest.allure.severity_level.MINOR)
     def test_function1(self):
         print("testcase1")
 
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test_function2(self):
         print("testcase2")
